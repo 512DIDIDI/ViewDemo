3279138bb94ef43b6173cc322d498910be12eaa9
dididi <yc512yc@163.com>
2020-06-02 17:35:50 +0800

    feat($Paint) paint

--- app/src/main/java/com/dididi/viewdemo/PaintView.kt
+++ app/src/main/java/com/dididi/viewdemo/PaintView.kt
@@ -6,7 +6,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import java.util.jar.Attributes
+import com.dididi.uiextlib.ext.getScreeSize
 
 
 /**
@@ -15,25 +15,64 @@ import java.util.jar.Attributes
  * @describe
  */
 
-class PaintView :View{
-    constructor(context:Context):super(context)
-    constructor(context: Context,attrs:AttributeSet):super(context,attrs)
-    constructor(context: Context,attrs:AttributeSet,defStyleAttr: Int):super(context,attrs,defStyleAttr)
+class PaintView : View {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
 
-    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.RED
+        style = Paint.Style.STROKE
+        strokeWidth = 10f
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.color = Color.RED
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = 10f
+        val point = context.getScreeSize()
+        val x = point.x.toFloat() / 2
+        val y = point.y.toFloat() / 2
         //这个坐标不是屏幕坐标 应该是View内的坐标
-        canvas?.drawCircle(1100f,650f,200f,paint)
-        canvas?.drawRect(900f,450f,1300f,850f,paint)
+        //园
+        canvas?.drawCircle(x, y, 200f, paint)
+        //矩形
+        canvas?.drawRect(x - 200f, y - 200f, x + 200f, y + 200f, paint)
         paint.strokeCap = Paint.Cap.SQUARE
         paint.strokeWidth = 50f
-        canvas?.drawPoint(1100f,650f,paint)
-        val points = floatArrayOf(0f, 0f, 800f, 650f, 1100f, 350f, 1400f, 650f, 1100f, 950f, 150f, 50f, 150f, 100f)
-        canvas?.drawPoints(points,2/*跳过前两个数*/,8/*共绘制四个点*/,paint)
+        //点
+        canvas?.drawPoint(x, y, paint)
+        val points = floatArrayOf(
+            0f,
+            0f,
+            x - 300f,
+            y,
+            x,
+            y - 300f,
+            x + 300f,
+            y,
+            x,
+            y + 300f,
+            150f,
+            50f,
+            150f,
+            100f
+        )
+        canvas?.drawPoints(points, 2/*跳过前两个数*/, 8/*共绘制四个点*/, paint)
+        paint.strokeWidth = 25f
+        val lines = floatArrayOf(
+            x, 0f, x, 2 * y,
+            0f, y, 2 * x, y
+        )
+        canvas?.apply {
+            //椭圆
+            drawOval(x - 300f, y - 300f, x + 300f, y + 300f, paint)
+            //线
+            drawLines(lines, paint)
+            //圆角矩形
+            drawRoundRect(x - 300f, y - 300f, x + 300f, y + 300f, 50f, 50f, paint)
+        }
     }
 }
